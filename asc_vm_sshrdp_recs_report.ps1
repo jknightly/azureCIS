7fe3545baa9874ce87cbfe84ad76eeddde4bda0f
John Knighlty <jknightly@azknightlys.com>
2019-06-07 15:10:04 -0700

    Add files via upload
    Export subscription ID and VMname for virtual machines with management ports open to the internet

---
+++ asc_vm_sshrdp_recs_report.ps1
@@ -0,0 +1,11 @@
+﻿get-azcontext -listavailable -pipelinevariable AzureRMSub | set-azcontext | foreach {
+$azsectask = get-azsecuritytask
+$azsectask | where RecommendationType -EQ  "Close management ports on your Virtual Machines" | foreach {
+    $_.ResourceId
+    $info = $_.ResourceId
+    $sub = $info.split('/')[-7]
+    $saname = $info.split('/')[-1]
+    $file = $sub + ',' + $saname
+    $file | Out-File ~/clouddrive/openrdpssh.csv -Append
+     }
+ }
