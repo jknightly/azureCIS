de6adb4509eb6d92c76de61ccc2861636675f705
John Knighlty <jknightly@azknightlys.com>
2019-06-07 14:42:16 -0700

    Add files via upload
    This script is meant to be executed in Azure CloudShell. It will dump a CSV file to clouddrive with all storage accounts across all subscriptions that are not compliant with CIS 3.1

---
+++ asc_storage_https_recs_report.ps1
@@ -0,0 +1,11 @@
+﻿get-azcontext -listavailable -pipelinevariable AzureRMSub | set-azcontext | foreach {
+$azsectask = get-azsecuritytask
+$azsectask | where RecommendationType -EQ  "Require secure transfer to storage account" | foreach {
+    $_.ResourceId
+    $info = $_.ResourceId
+    $sub = $info.split('/')[-7]
+    $saname = $info.split('/')[-1]
+    $file = $sub + ',' + $saname
+    $file | Out-File ~/clouddrive/sahttps.csv -Append
+     }
+ }
