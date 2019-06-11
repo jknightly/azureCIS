8846e72d50cfe33ff41910696a1d75253ce9a66a
John Knighlty <jknightly@azknightlys.com>
2019-06-11 11:54:14 -0700

    Update asc_storage_https_recs_report.ps1

--- asc_storage_https_recs_report.ps1
+++ asc_storage_https_recs_report.ps1
@@ -1,11 +1,10 @@
-﻿get-azcontext -listavailable -pipelinevariable AzureRMSub | set-azcontext | foreach {
-$azsectask = get-azsecuritytask
-$azsectask | where RecommendationType -EQ  "Require secure transfer to storage account" | foreach {
-    $_.ResourceId
-    $info = $_.ResourceId
-    $sub = $info.split('/')[-7]
-    $saname = $info.split('/')[-1]
-    $file = $sub + ',' + $saname
-    $file | Out-File ~/clouddrive/sahttps.csv -Append
-     }
- }
+get-azcontext -listavailable -pipelinevariable AzureRMSub | set-azcontext | foreach {
+    $azsectask = get-azsecuritytask
+    $azsectask | where RecommendationType -EQ  "Require secure transfer to storage account" | foreach {
+        Write-host "$($_.ResourceId)"
+        $_ | select `
+            @{n='Sub';e={$_.ResourceId.split('/')[-7]}}, `
+            @{n='SAName';e={$_.ResourceId.split('/')[-1]}}
+    } | export-csv ~/clouddrive/sahttps.csv -Append -NoTypeInformation
+}
+
