ca4ffe43b34b70444736a9664cbc2a991abbde33
John Knighlty <jknightly@azknightlys.com>
2019-06-11 11:31:16 -0700

    Update plublicaccessAZ.ps1

--- plublicaccessAZ.ps1
+++ plublicaccessAZ.ps1
@@ -1,3 +1,3 @@
 ﻿get-azcontext -listavailable -pipelinevariable AzureRMSub | set-azcontext | foreach {
     Get-AzStorageAccount | Get-AzStorageContainer -pipelinevariable azstorcont | `
-        where {$azstorcont.PublicAccess -NE "Off" | Out-File ~/clouddrive/publicstorage.txt}}
+        where {$azstorcont.PublicAccess -NE "Off" | Out-File ~/clouddrive/publicstorage.txt -append}}
