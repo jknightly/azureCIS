ba9855ba3478974ee15c24c6e63674362f1cac06
John Knighlty <jknightly@azknightlys.com>
2019-06-11 13:13:46 -0700

    Add files via upload
    Checks to see if at least 1 instance of network watcher is enabled on a each subscription.

---
+++ check_network_watcher_enabled.ps1
@@ -0,0 +1,4 @@
+﻿get-azcontext -listavailable -pipelinevariable AzureRMSub | set-azcontext | foreach {
+$watcher = get-aznetworkwatcher 
+if ($watcher -eq $null)
+    {$AzureRmsub | out-file ~/clouddrive/subs_watchernotenabled.csv -Append }}
