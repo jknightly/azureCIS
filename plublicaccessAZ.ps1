474fd53c6d8d8af253f71611b1245b58bf4bdad1
John Knighlty <jknightly@azknightlys.com>
2019-06-11 09:52:01 -0700

    Add files via upload
    reports on containers with public access enabled

---
+++ plublicaccessAZ.ps1
@@ -0,0 +1,3 @@
+﻿get-azcontext -listavailable -pipelinevariable AzureRMSub | set-azcontext | foreach {
+    Get-AzStorageAccount | Get-AzStorageContainer -pipelinevariable azstorcont | `
+        where {$azstorcont.PublicAccess -NE "Off" | Out-File ~/clouddrive/publicstorage.txt}}
