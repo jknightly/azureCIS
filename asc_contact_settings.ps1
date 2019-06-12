b5022072443b00cf9f7b439abed17b181274219c
John Knighlty <jknightly@azknightlys.com>
2019-06-12 15:11:30 -0700

    Add files via upload
    Check ASC contact info settings

---
+++ asc_contact_settings.ps1
@@ -0,0 +1,14 @@
+﻿get-azcontext -listavailable -pipelinevariable AzureRMSub | set-azcontext | foreach {
+$contact = get-azsecuritycontact
+if ($contact -eq $null)
+{$AzureRMsub.Subscription | Out-File -filepath ~/clouddrive/nocontanctinfo.txt -Append}
+if ($contact.email -eq $null)
+{ $AzureRMsub.Subscription | Out-File  -filepath ~/clouddrive/nocontactemail.txt -Append}
+if ($contact.phone -eq $null)
+{  $AzureRMsub.Subscription | Out-File  -filepath ~/clouddrive/nocontactphone.txt -Append}
+if ($contact.AlertNotifications -eq $null)
+{ $AzureRMsub.Subscription | Out-File -filepath ~/clouddrive/noalertnotifications.txt -Append}
+if ($contact.AlertstoAdmins -eq $null)
+{ $AzureRMsub.Subscription | Out-File  -filepath ~/clouddrive/nocalertstoadmins.txt -Append
+}
+}
