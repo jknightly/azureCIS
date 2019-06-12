7e098834292e9ef271cc932698f1de500fc78ff3
John Knighlty <jknightly@azknightlys.com>
2019-06-12 12:16:48 -0700

    Add files via upload
    output list of Azure Key Vaults without diagnostics settings enabled

---
+++ diagnostics_for_keyvault.ps1
@@ -0,0 +1,17 @@
+﻿get-azcontext -listavailable -pipelinevariable AzureRMSub | set-azcontext | foreach {
+
+    $azsectask = get-azsecuritytask
+
+    $azsectask | where RecommendationType -EQ  "Diagnostics logs in Key Vault should be enabled" | foreach {
+
+        Write-host "$($_.ResourceId)"
+
+        $_ | select `
+
+            @{n='Sub';e={$_.ResourceId.split('/')[-7]}}, `
+
+            @{n='Kvname';e={$_.ResourceId.split('/')[-1]}}
+
+    } | export-csv ~/clouddrive/sahttps.csv -Append -NoTypeInformation
+
+}
