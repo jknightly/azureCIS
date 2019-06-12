a3a86f8e060172f46624cff16e1379694319714f
John Knighlty <jknightly@azknightlys.com>
2019-06-12 13:03:14 -0700

    Add files via upload
    Check if log profile exists, retention settings are compliant and all regions including global are selected.

---
+++ logprofilesettings.ps1
@@ -0,0 +1,7 @@
+﻿get-azcontext -listavailable -pipelinevariable AzureRMSub | set-azcontext | foreach {
+$lp = get-azlogprofile
+$count = $lp.locations | measure-object 
+if ($count -lt '33') { $azurermsub | out-file ~/clouddrive/lp_not_all_regions.txt}
+if ($lp.id -eq $null) {$azurermsub | out-file ~/clouddrive/nologprofile.txt}
+if ($lp.retentionpolicy.enabled -eq 'False') {out-file ~/clouddrive/lP_no_retention.txt}
+    elseif ($lp.retentionpolicy.days -lt'365') {out-file ~/clouddrive/lP_short_retention.txt}}
