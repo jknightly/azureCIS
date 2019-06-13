d0c80191266c28eda21a012bbcb604859c0b1b7d
John Knighlty <jknightly@azknightlys.com>
2019-06-13 14:19:58 -0700

    Update asc_contact_settings.ps1

--- asc_contact_settings.ps1
+++ asc_contact_settings.ps1
@@ -1,14 +1,5 @@
-﻿get-azcontext -listavailable -pipelinevariable AzureRMSub | set-azcontext | foreach {
+get-azcontext -listavailable -pipelinevariable AzureRMSub | set-azcontext | foreach {
 $contact = get-azsecuritycontact
-if ($contact -eq $null)
-{$AzureRMsub.Subscription | Out-File -filepath ~/clouddrive/nocontanctinfo.txt -Append}
-if ($contact.email -eq $null)
-{ $AzureRMsub.Subscription | Out-File  -filepath ~/clouddrive/nocontactemail.txt -Append}
-if ($contact.phone -eq $null)
-{  $AzureRMsub.Subscription | Out-File  -filepath ~/clouddrive/nocontactphone.txt -Append}
-if ($contact.AlertNotifications -eq $null)
-{ $AzureRMsub.Subscription | Out-File -filepath ~/clouddrive/noalertnotifications.txt -Append}
-if ($contact.AlertstoAdmins -eq $null)
-{ $AzureRMsub.Subscription | Out-File  -filepath ~/clouddrive/nocalertstoadmins.txt -Append
-}
+if ($contact -eq $null -or $contact.email -eq $null -or $contact.phone -eq $null -or $contact.AlertNotifications -eq $null -or $contact.AlertstoAdmins -eq $null)
+    {AzureRMsub | out-file ~/clouddrive/ASC_contacts_not_set.csv -append}
 }
