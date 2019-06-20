6a8742ec6f2b1fcf405621dd2d94a96523256f42
John Knighlty <jknightly@azknightlys.com>
2019-06-20 13:45:40 -0700

    Create remediate_storage_https.ps1

---
+++ remediation/remediate_storage_https.ps1
@@ -0,0 +1,11 @@
+get-azcontext -listavailable -pipelinevariable AzureRMSub | set-azcontext | foreach {
+
+$azsectask = get-azsecuritytask
+
+$azsectask | where RecommendationType -EQ  "Require secure transfer to storage account" | foreach {
+
+     Set-AzStorageAccount -ResourceGroupName $_.resourceid.split('/')[-5] -accountname  $_.resourceid.split('/')[-1] -EnableHttpsTrafficOnly $true
+
+    }
+
+ }
