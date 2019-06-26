75e83e973fbb65e0fb5b96814e84bdb8fd51d21b
nomie788 <nquresh88@hotmail.com>
2019-06-26 10:34:03 -0500

    Add files via upload

---
+++ Audit_sqlDB_audit_set_On.ps1
@@ -0,0 +1,8 @@
+﻿# Login-AzAccount 
+
+$shellOutFilePath = "~/clouddrive/sqlaudit_status.csv"
+
+#loop through all subscriptions
+Get-AzContext -ListAvailable -PipelineVariable AzureRMSub | Set-AzContext | foreach {
+Get-AzSqlServer -ErrorAction Ignore | Get-AzSqlDatabase -pipelinevariable sqldb | Get-AzSqlDatabaseAuditing | Export-Csv $shellOutFilePath -Append -NoTypeInformation
+    }
