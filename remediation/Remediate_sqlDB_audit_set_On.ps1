4bdf11c483a50528323ad14512d9dcab222a7e03
nomie788 <nquresh88@hotmail.com>
2019-07-01 01:16:42 -0500

    Add files via upload

---
+++ remediation/Remediate_sqlDB_audit_set_On.ps1
@@ -0,0 +1,15 @@
+﻿# Login-AzAccount 
+
+
+$bloblstoragename = "dqlauditblob"
+
+    #loop through all subscriptions
+Get-AzContext -ListAvailable -PipelineVariable AzureRMSub | Set-AzContext | foreach {
+Get-AzSqlServer -ErrorAction Ignore | Get-AzSqlserverAuditing -ErrorAction Ignore -pipelinevariable dbaudit | `
+    where {$dbaudit.AuditState -eq "Disabled"} | foreach {
+    $dbaudit |  Set-AzSqlServerAuditing -State Enabled -StorageAccountName "dqlauditblob" -RetentionInDays 91
+
+    }
+    }
+
+
