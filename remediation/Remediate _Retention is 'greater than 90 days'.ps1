ace6e86726f0a962c5f0f74ed35403610224cc69
nomie788 <nquresh88@hotmail.com>
2019-07-01 01:57:52 -0500

    Add files via upload

---
+++ remediation/Remediate _Retention is 'greater than 90 days'.ps1
@@ -0,0 +1,14 @@
+﻿
+
+    #loop through all subscriptions
+Get-AzContext -ListAvailable -PipelineVariable AzureRMSub | Set-AzContext | foreach {
+Get-AzSqlServer -ErrorAction Ignore | Get-AzSqlserverAuditing -ErrorAction Ignore -pipelinevariable dbaudit | `
+    where {$dbaudit.RetentionInDays -le 90} | foreach {
+    If($dbaudit.RetentionInDays -le 90){
+    $dbaudit |  Set-AzSqlServerAuditing -State Enabled -RetentionInDays 91
+
+    }
+    }
+    }
+
+ 
