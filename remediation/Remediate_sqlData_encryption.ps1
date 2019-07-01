fcb4fab3fad95763eea56b37f276df659a67a0a9
nomie788 <nquresh88@hotmail.com>
2019-07-01 03:43:52 -0500

    Add files via upload

---
+++ remediation/Remediate_sqlData_encryption.ps1
@@ -0,0 +1,11 @@
+﻿
+
+#loop through all subscriptions
+Get-AzContext -ListAvailable -PipelineVariable AzureRMSub | Set-AzContext | foreach {
+Get-AzSqlServer -ErrorAction Ignore | Get-AzSqlDatabase -pipelinevariable sqldb | Get-AzSqlDatabaseTransparentDataEncryption -ErrorAction Ignore -PipelineVariable sqltde | `
+    where {$sqltde.State -eq "Disabled"} | foreach {
+    $sqltde | Set-AzSqlDatabaseTransparentDataEncryption  -State Enabled -ErrorAction Ignore
+
+    }}
+
+
