1611aa2b5d7520e76607645a97f3e92607f35a54
nomie788 <nquresh88@hotmail.com>
2019-07-04 17:39:03 -0500

    Update Remediate_PostGre_con_throttling.ps1

--- remediation/Remediate_PostGre_con_throttling.ps1
+++ remediation/Remediate_PostGre_con_throttling.ps1
@@ -1,16 +1,13 @@
-﻿Get-AzContext -ListAvailable -PipelineVariable AzuremSub | Set-AzContext |
-foreach{
-    $postGre = $null;
-    $postGre = Get-AzResource -ResourceType Microsoft.DBforPostgreSQL/servers -ExpandProperties -ErrorAction Ignore;
-    if($postGre){
-         $logcon = Get-AzResource -ResourceGroupName $PostGre.ResourceGroupName `
+Get-AzContext -ListAvailable -PipelineVariable AzuremSub | Set-AzContext |foreach{
+Get-AzResource -ResourceType Microsoft.DBforPostgreSQL/servers -ExpandProperties -ErrorAction Ignore -PipelineVariable PostGre | foreach{
+$logcon = Get-AzResource -ResourceGroupName $PostGre.ResourceGroupName `
         -ResourceType Microsoft.DBforPostgreSQL/servers/configurations `
         -ResourceName (-join $PostGre.Name + '/connection_throttling') -ApiVersion 2017-12-01;
-        If($logcon.Properties.Value -eq "OFF"){
+If($logcon.Properties.Value -eq "OFF"){
             $logcon.Properties.Value = 'on';
             $logcon |  Set-AzResource -ApiVersion 2017-12-01 -Force;
         }
     }
-}
+    }
 
     
