96b790bbd03416c5c1e159886844950687c0f5d9
nomie788 <nquresh88@hotmail.com>
2019-07-04 18:19:09 -0500

    Update Remediate_PostGre_log_retention_days.ps1

--- remediation/Remediate_PostGre_log_retention_days.ps1
+++ remediation/Remediate_PostGre_log_retention_days.ps1
@@ -1,9 +1,6 @@
-﻿Get-AzContext -ListAvailable -PipelineVariable AzuremSub | Set-AzContext |
-foreach{
-    $postGre = $null;
-    $postGre = Get-AzResource -ResourceType Microsoft.DBforPostgreSQL/servers -ExpandProperties -ErrorAction Ignore;
-    if($postGre){
-         $logcon = Get-AzResource -ResourceGroupName $PostGre.ResourceGroupName `
+Get-AzContext -ListAvailable -PipelineVariable AzuremSub | Set-AzContext |foreach{
+Get-AzResource -ResourceType Microsoft.DBforPostgreSQL/servers -ExpandProperties -ErrorAction Ignore -PipelineVariable PostGre | foreach{
+$logcon = Get-AzResource -ResourceGroupName $PostGre.ResourceGroupName `
         -ResourceType Microsoft.DBforPostgreSQL/servers/configurations `
         -ResourceName (-join $PostGre.Name + '/log_retention_days') -ApiVersion 2017-12-01;
         If($logcon.Properties.Value -le 3){
@@ -11,5 +8,5 @@ foreach{
             $logcon |  Set-AzResource -ApiVersion 2017-12-01 -Force;
         }
     }
-}
+    }
 
