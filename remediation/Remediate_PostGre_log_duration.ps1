5b8aced554081d292c20e13dbedb93609f994dd2
nomie788 <nquresh88@hotmail.com>
2019-07-04 18:51:39 -0500

    Add files via upload

---
+++ remediation/Remediate_PostGre_log_duration.ps1
@@ -0,0 +1,12 @@
+﻿Get-AzContext -ListAvailable -PipelineVariable AzuremSub | Set-AzContext |foreach{
+Get-AzResource -ResourceType Microsoft.DBforPostgreSQL/servers -ExpandProperties -ErrorAction Ignore -PipelineVariable PostGre| foreach{
+         $logcon = Get-AzResource -ResourceGroupName $PostGre.ResourceGroupName `
+        -ResourceType Microsoft.DBforPostgreSQL/servers/configurations `
+        -ResourceName (-join $PostGre.Name + '/log_duration') -ApiVersion 2017-12-01;
+        If($logcon.Properties.Value -eq "OFF"){
+            $logcon.Properties.Value = 'on';
+            $logcon |  Set-AzResource -ApiVersion 2017-12-01 -Force;
+        }
+
+    }
+}
