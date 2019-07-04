a9a853bd5f25c86d7c916ab2e91a3f95b5109c43
nomie788 <nquresh88@hotmail.com>
2019-07-04 13:16:29 -0500

    Add files via upload

---
+++ remediation/Remediate_PostGre_log_checkpoints.ps1
@@ -0,0 +1,14 @@
+﻿Get-AzContext -ListAvailable -PipelineVariable AzuremSub | Set-AzContext |
+foreach{
+    $postGre = $null;
+    $postGre = Get-AzResource -ResourceType Microsoft.DBforPostgreSQL/servers -ExpandProperties -ErrorAction Ignore;
+    if($postGre){
+         $logcon = Get-AzResource -ResourceGroupName $PostGre.ResourceGroupName `
+        -ResourceType Microsoft.DBforPostgreSQL/servers/configurations `
+        -ResourceName (-join $PostGre.Name + '/log_checkpoints') -ApiVersion 2017-12-01;
+        If($logcon.Properties.Value -eq "OFF"){
+            $logcon.Properties.Value = 'on';
+            $logcon |  Set-AzResource -ApiVersion 2017-12-01 -Force;
+        }
+    }
+}
