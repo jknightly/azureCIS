40e9743ee5638f5712497eec651dbf1967f72207
nomie788 <nquresh88@hotmail.com>
2019-07-04 16:19:25 -0500

    Add files via upload

---
+++ remediation/Remediate_PostGre_SSLEnforcement.ps1
@@ -0,0 +1,17 @@
+﻿    $postGre = $null;
+    $postGre = Get-AzResource -ResourceType Microsoft.DBforPostgreSQL/servers -ExpandProperties -ErrorAction Ignore;
+
+
+Get-AzContext -ListAvailable -PipelineVariable AzuremSub | Set-AzContext |
+foreach{
+ #   $postGre = $null;
+ #   $postGre = Get-AzResource -ResourceType Microsoft.DBforPostgreSQL/servers -ExpandProperties -ErrorAction Ignore;
+    if($postGre){
+        If($PostGre.properties.sslEnforcement -eq "Disabled"){
+            $PostGre.Properties.sslEnforcement = 'Enabled';
+           $PostGre |  Set-AzResource  -Force;
+        }
+    }
+}
+
+    
