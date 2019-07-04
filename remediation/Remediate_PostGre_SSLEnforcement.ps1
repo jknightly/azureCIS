a5e6f51b70696f15a49c55cec688bdc9a2eaf1fa
nomie788 <nquresh88@hotmail.com>
2019-07-04 17:19:24 -0500

    Update Remediate_PostGre_SSLEnforcement.ps1

--- remediation/Remediate_PostGre_SSLEnforcement.ps1
+++ remediation/Remediate_PostGre_SSLEnforcement.ps1
@@ -1,17 +1,10 @@
-﻿    $postGre = $null;
-    $postGre = Get-AzResource -ResourceType Microsoft.DBforPostgreSQL/servers -ExpandProperties -ErrorAction Ignore;
-
-
-Get-AzContext -ListAvailable -PipelineVariable AzuremSub | Set-AzContext |
-foreach{
- #   $postGre = $null;
- #   $postGre = Get-AzResource -ResourceType Microsoft.DBforPostgreSQL/servers -ExpandProperties -ErrorAction Ignore;
-    if($postGre){
+Get-AzContext -ListAvailable -PipelineVariable AzuremSub | Set-AzContext |foreach{
+Get-AzResource -ResourceType Microsoft.DBforPostgreSQL/servers -ExpandProperties -ErrorAction Ignore -PipelineVariable PostGre | foreach{
         If($PostGre.properties.sslEnforcement -eq "Disabled"){
-            $PostGre.Properties.sslEnforcement = 'Enabled';
+           $PostGre.Properties.sslEnforcement = 'Enabled';
            $PostGre |  Set-AzResource  -Force;
         }
-    }
+}
 }
 
     
