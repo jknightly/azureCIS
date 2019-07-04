27cb2dfe005dd645dcd10c75c9a4ef1defeef238
nomie788 <nquresh88@hotmail.com>
2019-07-04 18:06:07 -0500

    Update Remediate_PostGre_log_disconnections.ps1

--- remediation/Remediate_PostGre_log_disconnections.ps1
+++ remediation/Remediate_PostGre_log_disconnections.ps1
@@ -1,15 +1,11 @@
-﻿Get-AzContext -ListAvailable -PipelineVariable AzuremSub | Set-AzContext |
-foreach{
-    $postGre = $null;
-    $postGre = Get-AzResource -ResourceType Microsoft.DBforPostgreSQL/servers -ExpandProperties -ErrorAction Ignore;
-    if($postGre){
-        $logcon = Get-AzResource -ResourceGroupName $PostGre.ResourceGroupName `
+Get-AzContext -ListAvailable -PipelineVariable AzuremSub | Set-AzContext |foreach{
+Get-AzResource -ResourceType Microsoft.DBforPostgreSQL/servers -ExpandProperties -ErrorAction Ignore -PipelineVariable PostGre | foreach{
+$logcon = Get-AzResource -ResourceGroupName $PostGre.ResourceGroupName `
         -ResourceType Microsoft.DBforPostgreSQL/servers/configurations `
         -ResourceName (-join $PostGre.Name + '/log_disconnections') -ApiVersion 2017-12-01;
-        If($logcon.Properties.Value -eq "OFF"){
+If($logcon.Properties.Value -eq "OFF"){
             $logcon.Properties.Value = 'on';
             $logcon |  Set-AzResource -ApiVersion 2017-12-01 -Force;
         }
-        
     }
-}
+    }
