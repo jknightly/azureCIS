2d0edaa200c369f46a48c1703d1ad19011912d6a
nomie788 <nquresh88@hotmail.com>
2019-07-09 17:26:31 -0500

    Update Remediate_PostGre_log_duration.ps1

--- remediation/Remediate_PostGre_log_duration.ps1
+++ remediation/Remediate_PostGre_log_duration.ps1
@@ -5,6 +5,7 @@ Get-AzResource -ResourceType Microsoft.DBforPostgreSQL/servers -ExpandProperties
         -ResourceName (-join $PostGre.Name + '/log_duration') -ApiVersion 2017-12-01;
         If($logcon.Properties.Value -eq "OFF"){
             $logcon.Properties.Value = 'on';
+            $logcon.Properties.source = 'user-override';
             $logcon |  Set-AzResource -ApiVersion 2017-12-01 -Force;
         }
 
