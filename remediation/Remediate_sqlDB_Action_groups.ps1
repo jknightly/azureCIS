61b30d32907f79556a4ee1e05f016b7bb8f67645
nomie788 <nquresh88@hotmail.com>
2019-07-09 00:00:49 -0500

    Add files via upload

---
+++ remediation/Remediate_sqlDB_Action_groups.ps1
@@ -0,0 +1,15 @@
+﻿    #loop through all subscriptions
+Get-AzContext -ListAvailable -PipelineVariable AzureRMSub | Set-AzContext | foreach {
+Get-AzSqlServer  -ErrorAction Ignore | Get-AzSqlserverAuditing -ErrorAction Ignore -pipelinevariable dbaudit | `
+    where {$dbaudit.AuditActionGroup -notmatch "SUCCESSFUL_DATABASE_AUTHENTICATION_GROUP
+FAILED_DATABASE_AUTHENTICATION_GROUP
+BATCH_COMPLETED_GROUP"} | foreach {
+    $dbaudit| Set-AzSqlServerAuditing -State Enabled -AuditActionGroup @('SUCCESSFUL_DATABASE_AUTHENTICATION_GROUP', 'FAILED_DATABASE_AUTHENTICATION_GROUP', 'BATCH_COMPLETED_GROUP')
+
+    }
+    }
+
+    ###### Informational####
+# WARNING: NOTE : Go to https://aka.ms/azps-changewarnings for steps to suppress this breaking change warning, and other information on breaking changes in Azure PowerShell.
+# WARNING: Breaking changes in the cmdlet 'Get-AzSqlServerAuditing' :
+# WARNING:  - The cmdlet 'GetAzSqlServerAudit' is replacing this cmdlet.
