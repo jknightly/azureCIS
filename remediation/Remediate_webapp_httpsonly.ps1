2c851804c8769495df4c8d463aac96c6b3e5a424
nomie788 <nquresh88@hotmail.com>
2019-07-11 10:19:57 -0500

    Add files via upload

---
+++ remediation/Remediate_webapp_httpsonly.ps1
@@ -0,0 +1,11 @@
+﻿# Enforce web app redirects all HTTP traffic to HTTPS in Azure App Service
+
+
+            #loop through all subscriptions
+Get-AzContext -ListAvailable -PipelineVariable AzureRMSub | Set-AzContext | foreach {
+Get-AzWebApp -ErrorAction Ignore -PipelineVariable webapp | `
+    where {$webapp.HttpsOnly -ne $true} | foreach {
+    $webapp |  Set-Azwebapp -ResourceGroupName $webapp.ResourceGroup -HttpsOnly $true 
+    }
+    }
+
