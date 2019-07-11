64004db6409b75ccc8f1effeafc4ba49760fb5b2
nomie788 <nquresh88@hotmail.com>
2019-07-11 10:20:48 -0500

    Add files via upload

---
+++ audit-webapp_httpsonly.ps1
@@ -0,0 +1,13 @@
+﻿# Audit web app redirects all HTTP traffic to HTTPS in Azure App Service
+
+$shellOutFilePath = "~/clouddrive/webapp_redirect_status.csv"
+
+Get-AzContext -ListAvailable -PipelineVariable AzureRMSub | Set-AzContext | foreach{
+Get-AzWebApp -ErrorAction Ignore -PipelineVariable webapp | select `
+    @{n='SubscriptionName';e={$AzureRMSub.Name}}, `
+    @{n='ResourceGroupName';e={$webapp.ResourceGroup}}, `
+    @{n='Name';e={$webapp.Name}}, `
+    @{n='HttptoHttpsOnly';e={$webapp.HttpsOnly}}
+    } | Export-Csv $shellOutFilePath -Append -NoTypeInformation
+    
+
