82130784b5cdb515faa58c62522ccd5b40a19b9f
nomie788 <nquresh88@hotmail.com>
2019-07-11 13:54:31 -0500

    Add files via upload

---
+++ remediation/Remediate_allow_ingress.ps1
@@ -0,0 +1,12 @@
+﻿# remove sqlserverfirewallrul "allowallwindowsazureips"
+
+    #loop through all subscriptions
+Get-AzContext -ListAvailable -PipelineVariable AzureRMSub | Set-AzContext | foreach {
+Get-AzSqlServer -ErrorAction Ignore | Get-AzSqlServerFirewallRule  -pipelinevariable sqlfw | `
+    where {$sqlfw.FirewallRuleName -eq "AllowAllWindowsAzureIps"} | foreach {
+    $sqlfw | remove-AzSqlServerFirewallRule -FirewallRuleName "AllowAllWindowsAzureIps"
+
+    }
+    }
+
+
