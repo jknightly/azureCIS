6cdd33c74e3f5fa5c559e994916f4da350d4ba5b
nomie788 <nquresh88@hotmail.com>
2019-07-11 13:21:12 -0500

    Add files via upload

---
+++ remediation/Remediate_webapp_netframework.ps1
@@ -0,0 +1,11 @@
+﻿    # Remediate web app is using the latest version of .Netframework
+    #latest version 
+$latestversion = "v4.0"
+            #loop through all subscriptions
+Get-AzContext -ListAvailable -PipelineVariable AzureRMSub | Set-AzContext | foreach {
+Get-AzWebApp -ErrorAction Ignore -PipelineVariable webapp | `
+    where {$webapp.SiteConfig.NetFrameworkVersion -ne $latestversion} | foreach {
+    $webapp |  Set-Azwebapp -ResourceGroupName $webapp.ResourceGroup -NetFrameworkVersion $latestversion 
+    }
+    }
+
