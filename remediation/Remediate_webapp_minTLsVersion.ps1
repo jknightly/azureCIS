a591c602cce78d4ead38830669d61403d3cb384b
nomie788 <nquresh88@hotmail.com>
2019-07-12 13:27:50 -0500

    Add files via upload

---
+++ remediation/Remediate_webapp_minTLsVersion.ps1
@@ -0,0 +1,13 @@
+﻿
+Get-AzContext -ListAvailable -PipelineVariable AzuremSub | Set-AzContext |foreach{
+Get-AzResource -ResourceType Microsoft.Web/sites -ErrorAction Ignore -PipelineVariable webapp| foreach{
+         $mintls = Get-AzResource -ResourceGroupName $webapp.ResourceGroupName `
+        -ResourceType Microsoft.Web/sites/config `
+        -ResourceName (-join $webapp.Name + '/web') -ApiVersion 2018-02-01;
+        If($mintls.Properties.minTlsVersion -ne "1.2"){
+            $mintls.Properties.minTlsVersion = '1.2';
+            $mintls |  Set-AzResource -ApiVersion 2018-02-01 -Force;
+        }
+
+    }
+}
