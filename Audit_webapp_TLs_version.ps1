5a24e3632bc9237f34929ec2d4e9f5b55ea6d031
nomie788 <nquresh88@hotmail.com>
2019-07-12 13:51:56 -0500

    Add files via upload

---
+++ Audit_webapp_TLs_version.ps1
@@ -0,0 +1,14 @@
+﻿# Audit web app is using the latest version of TLS encryption
+
+$shellOutFilePath = "~/clouddrive/webapp_minTLs_version.csv"
+
+Get-AzContext -ListAvailable -PipelineVariable AzureRMSub -ErrorAction Ignore | Set-AzContext | foreach {
+Get-AzResource -ResourceType Microsoft.Web/sites -ErrorAction Ignore -PipelineVariable webapp | select `
+    @{n='SubscriptionName';e={$AzureRMSub.Name}}, `
+    ResourceGroupName,Name, `
+    @{n='minTlsVersion';e={(Get-AzResource -ResourceGroupName newscriptrg -ResourceType Microsoft.Web/sites/config -ResourceName (-join $webapp.Name + "/web") -ApiVersion 2018-02-01).Properties.minTlsVersion}}
+ 
+} | Export-CSV $shellOutFilePath -NoTypeInformation 
+
+
+
