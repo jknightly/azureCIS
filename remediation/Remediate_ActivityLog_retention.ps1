dea61e032ac45f815fa186ef2f90a59e92843fb0
nomie788 <nquresh88@hotmail.com>
2019-07-13 22:17:27 -0500

    Add files via upload

---
+++ remediation/Remediate_ActivityLog_retention.ps1
@@ -0,0 +1,13 @@
+﻿# Remediate Log retention is 365 Days or more 
+# 365 is the limit
+
+    #loop through all subscriptions
+Get-AzContext -ListAvailable -PipelineVariable AzureRMSub | Set-AzContext | foreach {
+Get-AzLogProfile -ErrorAction Ignore -pipelinevariable log | `
+    where {$log.RetentionPolicy.Days -le 365} | foreach {
+    If($log.RetentionPolicy.Days -le 365){
+ #   $log.RetentionPolicy.Days = "366"
+    $log | Add-AzLogProfile -Name $log.Name -StorageAccountId $log.StorageAccountId -Location $log.Locations -RetentionInDays 365 -Category Write,Delete,Action
+    }
+    }
+    }
