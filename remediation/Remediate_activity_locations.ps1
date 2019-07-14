611f9f9fac85c10df5db658ba3c922f9c72bccf1
nomie788 <nquresh88@hotmail.com>
2019-07-13 23:00:19 -0500

    Add files via upload

---
+++ remediation/Remediate_activity_locations.ps1
@@ -0,0 +1,12 @@
+﻿# Remediate Locations
+
+
+$locations = (Get-AzLocation).Location
+$locations += "global"
+        #loop through all subscriptions
+Get-AzContext -ListAvailable -PipelineVariable AzureRMSub | Set-AzContext | foreach {
+Get-AzLogProfile -ErrorAction Ignore -pipelinevariable log | `
+    where {$log.locations -notmatch $locations} | foreach {
+    $log | Add-AzLogProfile -Name $log.Name -StorageAccountId $log.StorageAccountId -Location $locations -RetentionInDays 365 -Category Write,Delete,Action
+    }
+    }
